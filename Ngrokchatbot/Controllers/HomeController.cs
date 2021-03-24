e0487403877f3275c689e2e94e3621f0a4a64a54
Pratik Patil <pratik@sdcdpu.local>
2021-03-24 11:38:42 +0530

    Last checking

---
+++ Ngrokchatbot/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Ngrokchatbot.Models;
+
+namespace Ngrokchatbot.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public string Webhook([FromQuery(Name = "hub.mode")] string mode,
+
+          [FromQuery(Name = "hub.challenge")] string challenge,
+          [FromQuery(Name = "hub.verify_token")] string verify_token)
+        {
+            var json = Request.Query;
+
+            if (verify_token.Equals("my_token_is_great"))
+            {
+                return challenge;
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
